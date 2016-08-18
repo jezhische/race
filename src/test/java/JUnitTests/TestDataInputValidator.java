3db48b8aff9550fa1464bac224c99bd950d5a620
jezhische <jezhische@gmail.com>
2016-08-18 13:48:28 +0300

    some fixing after failed merge

--- src/test/java/JUnitTests/TestDataInputValidator.java
+++ src/test/java/JUnitTests/TestDataInputValidator.java
@@ -13,9 +13,6 @@ import static org.junit.Assert.*;
  * Created by Ежище on 14.08.2016.
  */
 public class TestDataInputValidator {
-//    DataInputValidator validator = new DataInputValidator();
-//    FileToString outputMsg = new FileToString();
-//    String msg = "some message";
 
     private DataInputValidator validator;
     private OutToFileRedirect sysOut;
@@ -34,9 +31,7 @@ public class TestDataInputValidator {
     @After
     public void tearDown() {
         validator = null;
-<<<<<<< HEAD
 //        sysOut.redirectOut().close();
-//        sysOut = null;
         outputMsg = null;
         msg = null;
     }
@@ -44,32 +39,15 @@ public class TestDataInputValidator {
     public static void logout() {
         System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out)));
     }
-=======
-//        try {
-//            sysOut.redirectOut().close();
-//        }
-//        catch(Exception e) {
-//            e.printStackTrace();
-//        }
-        sysOut = null;
-        outputMsg = null;
-        msg = null;
-    }
-//    @AfterClass
-//    public static void logout() {
-//        System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out)));
-//    }
->>>>>>> 489574ffcfbe4beb23906cbdb4962785be1a3311
 
     @Test
     public void testBreakWithAppendixPrintingFirstErr() throws ErrCountCauseException, IOException {
         sysOut.redirectOut();
         validator.breakWithAppendixPrinting(msg);
-<<<<<<< HEAD
         //TODO: полное равенство не срабатывает, потому что в файл в конце печатается еще куча пустых символов:
-        String message = outputMsg.readFileToString(new File("src\\main\\resources\\testSupport\\output.txt"));
 //        assertTrue(outputMsg.readFileToString(new File("src\\main\\resources\\testSupport\\output.txt"))
 //                .equals(msg + "\r\n" + ERR_COUNT_FIRST_LEVEL_MSG.getMessage() + "\r\n"));
+        String message = outputMsg.readFileToString(new File("src\\main\\resources\\testSupport\\output.txt"));
         assertTrue(message.contains(msg));
         assertTrue(message.contains(ERR_COUNT_FIRST_LEVEL_MSG.getMessage()));
     }
@@ -109,15 +87,6 @@ public class TestDataInputValidator {
         // TODO: из-за пробрасывания исключения все вот эти месседжи все равно не учитываются. Как их проверить?
         assertTrue(message.contains(msg));
         assertTrue(message.contains(ERR_COUNT_LAST_LEVEL_MSG.getMessage()));
-=======
-        validator.breakWithAppendixPrinting(msg);
-        validator.breakWithAppendixPrinting(msg);
-String message = outputMsg.readFileToString(new File("src\\main\\resources\\testSupport\\output.txt"));
-//        assertTrue(outputMsg.readFileToString(new File("src\\main\\resources\\testSupport\\output.txt"))
-//                .equals(msg + "\n" + ERR_COUNT_FIRST_LEVEL_MSG.getMessage() + "\n"));
-        assertTrue(message.contains(msg));
-        assertTrue(message.contains(ERR_COUNT_FIRST_LEVEL_MSG.getMessage()));
->>>>>>> 489574ffcfbe4beb23906cbdb4962785be1a3311
     }
 
 }
--- src/test/java/testSupport/OutToFileRedirect.java
+++ src/test/java/testSupport/OutToFileRedirect.java
@@ -2,10 +2,6 @@ package testSupport;
 
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-<<<<<<< HEAD
-import java.io.FileWriter;
-=======
->>>>>>> 489574ffcfbe4beb23906cbdb4962785be1a3311
 import java.io.PrintStream;
 
 /**
@@ -14,11 +10,7 @@ import java.io.PrintStream;
 public class OutToFileRedirect {
     public PrintStream redirectOut() {
         try  {
-<<<<<<< HEAD
             PrintStream output = new PrintStream(new FileOutputStream("src\\main\\resources\\testSupport\\output.txt", false));
-=======
-            PrintStream output = new PrintStream(new FileOutputStream("src\\main\\resources\\testSupport\\output.txt"));
->>>>>>> 489574ffcfbe4beb23906cbdb4962785be1a3311
             System.setOut(output);
             return System.out;
         } catch (FileNotFoundException e) {
