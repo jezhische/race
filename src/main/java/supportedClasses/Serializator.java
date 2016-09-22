2eece3d16284461defdb86ba2dbc0b2f57ae572a
jezhische <jezhische@gmail.com>
2016-09-22 10:59:00 +0300

    Serializator.java to Callable! It's run at last...

--- src/main/java/supportedClasses/Serializator.java
+++ src/main/java/supportedClasses/Serializator.java
@@ -8,13 +8,12 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 /**
  * Created by Ежище on 03.09.2016.
  */
-public class Serializator implements Runnable {
+public class Serializator implements Callable<Serializator> {
 
     /** list of the cars ready to race: */
     private ArrayList<Vehicle> carList = new ArrayList<>();
@@ -22,15 +21,17 @@ public class Serializator implements Runnable {
     /** counter of the serializing cars: */
     private int counter;
 
+    private volatile int threadCounter;
+
     /** for specification the path to the cars serialization: */
     private File dir = new File("src\\main\\resources\\serialStore");
 
-    Serializator(ArrayList<Vehicle> carList, int counter) {
+    public Serializator(ArrayList<Vehicle> carList, int counter) {
         this.carList = carList;
         this.counter = counter;
     }
 
-    Serializator() {
+    public Serializator() {
     }
 
     /** to create required directory for serialized files writing and to delete all the files there if it exists: */
@@ -60,52 +61,69 @@ public class Serializator implements Runnable {
 
     /** to serialize results of the race in run(): */
     @Override
-    public void run() { //TODO: стереть все выводы в консоль
-        if (fileDealer(dir)) {
-            Vehicle car = carList.get(counter);
+    public Serializator call() throws Exception { //TODO: стереть все выводы в консоль
+        Vehicle car = carList.get(counter);
+        File file = new File(String.format("src\\main\\resources\\serialStore\\log%d_%s_%s.dat",
+                counter, car.getName(), car.getMarker()));
+//        if (fileDealer(dir)) {
+        fileDealer(dir);
             try (ObjectOutputStream serialize = new ObjectOutputStream
-                    (new FileOutputStream(String.format("src\\main\\resources\\serialStore\\log%d_%s_%s.dat",
-                            counter, car.getName(), car.getMarker())))) {
+                    (new FileOutputStream(file))) {
                 car.goVehicle();
                 System.out.printf("внутри: %s %5.1f %7.2f\n", car.getName(), car.getRegisteredTime(), car.getAverageSpeed());
                 serialize.writeObject(car);
                 serialize.flush();
+                increment();
             } catch (IOException e) {
                 System.out.println("выкинуло!");
                 e.printStackTrace();
             }
             System.out.println(Thread.currentThread().getName());
             System.out.printf("снаружи: %s %5.1f %7.2f\n", car.getName(), car.getRegisteredTime(), car.getAverageSpeed());
-        }
+//        }
+        return new Serializator(carList, counter);
     }
 
     /**
      * to create some concurrent streams
      */
-    public void go(ArrayList<Vehicle> carList) {
+    public void go(ArrayList<Vehicle> carList) throws ExecutionException, InterruptedException {
         this.carList = carList;
-        ExecutorService executor = Executors.newFixedThreadPool(carList.size());
-        for (counter = 0; counter < carList.size(); counter++) {
-            Thread thread = new Thread(new Serializator(carList, counter), "Thread " + String.valueOf(counter));
-            executor.execute(thread);
-//            try {
-////                thread.join();
-//                thread.sleep(100);
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
+        if (fileDealer(dir)) {
+            ExecutorService executor = Executors.newFixedThreadPool(carList.size());
+            for (counter = 0; counter < carList.size(); counter++) {
+                Future threadDetector = executor.submit(new Serializator(carList, counter));
+                try {         // если дать поспать подольше, сенсоры заработают, но записывается только последний файл
+                Thread.sleep(3);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+                if (threadDetector.isDone())
+                    increment();
+//                Serializator serializator = (Serializator)threadDetector.get();
+//                serializator.increment();
+            }
+            executor.shutdown();
+            if (executor.isShutdown()) {
+                printWrittenFilesCount();
+                printThreadsCount();
+            }
+
         }
-        executor.shutdown();
     }
 
-//    private static void startThreads(Thread[] threads) { // rejected
-//        for (Thread t : threads)
-//            t.start();
-//    }
-
+    private void increment() {
+        threadCounter++;
+    }
+    public void printThreadsCount() {
+        System.out.println("Запущено потоков: " + threadCounter);
+    }
+    private void printWrittenFilesCount() {
+        System.out.println("Сериализовано объектов: " + dir.listFiles().length);
+    }
 
     // TODO: потом уничтожить main
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         InitialDataFileReader readFile = new InitialDataFileReader(new File("src//main//resources//pilotProbesData//Probe4.txt"));
         ArrayList<Vehicle> probe = readFile.readArgsFromFile();
 //        for (int counter = 0; counter < probe.size(); counter++) {
@@ -119,5 +137,6 @@ public class Serializator implements Runnable {
 
         Serializator ser = new Serializator();
         ser.go(probe);
+//        ser.printThreadsCount();
     }
 }
